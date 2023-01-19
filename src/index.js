a0ef66c240b3d245eba4ff6b433917f3f4f00757
Yauheni Kadyrau <7077634@gmail.com>
2023-01-19 20:07:25 +0300

    cart mode

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
 
-console.log(store.getState())
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <BrowserRouter>
--- src/redux/api.js
+++ src/redux/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const pizzasAPI = {
     getPizzas(activeCategory,activeSortBy) {
-        return axios.get(`http://localhost:3001/pizzas?${activeCategory!==null?`category=${activeCategory}`:''}&_sort=${activeSortBy.type}&_order=${activeSortBy.order}`)
+        return axios.get(`pizzas?${activeCategory!==null?`category=${activeCategory}`:''}&_sort=${activeSortBy.type}&_order=${activeSortBy.order}`)
     }
 }
